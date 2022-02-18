028b5a566b7b97db031dc4b4a1ae271ee085a57d
Lana-abdel <abdelml2@tcnj.edu>
2022-02-17 21:23:29 -0500

    edited the runnable_class function to accept a hash to track each sections availability and fixed a bug that wouldn't add students in other sections

--- easy_enroll.rb
+++ easy_enroll.rb
@@ -2,17 +2,19 @@ require 'csv'
 require './student.rb'
 require './courses.rb'
 class Easyenroll
-
-  def get_students(studentInfo)
+  
+  def get_students(studentInfo) 
+    #this function will get and intialize student objects from the csv file with student info to be used when registering 
     students = []
     CSV.read(studentInfo, headers: true).each do |student|
       students <<  Student.new(id: student[0], year: student[1], completed_courses: student[2].split(', '),
-                               sems_left:student[3], desired_courses: student[4] , first: student[5], second: student[6], third: student[7])
+      sems_left:student[3], desired_courses: student[4] , first: student[5], second: student[6], third: student[7])
     end
     students
   end
-
-  def sort(array= get_students)
+  
+  def sort(array= get_students) 
+    #this function sorts all the students by the priority, the sorted list will be printed for all the student information in output2.csv 
     for i in 1...(array.length)
       j = i
       while j > 0
@@ -28,46 +30,44 @@ class Easyenroll
     end
     return array
   end
-
-  def get_courses(courseInfo)
+  
+  def get_courses(courseInfo) 
+    #this function get and intialize course objects from the csv file with course info to be used when registering 
     courses = {}
-    #courseInfo = gets.chomp
     CSV.read(courseInfo, headers: true).each do |course|
       num_sectionstest  = course[1].to_i
       courses[course[0]] = Course.new(number: course[0], sections: Array.new(num_sectionstest){ [] }, min: course[2].to_i, max: course[3].to_i, num_sections: num_sectionstest)
     end
     courses
   end
-
+  
   def course_registration(studentInfo, courseInfo)
+    #this function will create student and course objects register students in their courses 
     students= get_students(studentInfo)
     courses = get_courses(courseInfo)
+    #will sort the students before we process the information 
     students = sort(students)
-    ###
+    #for each student we will enroll students in a course 
     students.each do |student|
       requested_courses = [student.first, student.second, student.third]
       enrolled = []
       requested_courses.each do |request|
-        #check if already enrolled
-        if enrolled.include?(request)
+        #check if already enrolled or if they have already been enrolled in 2 courses or if they had 0 as their desired number of courses 
+        if enrolled.include?(request) || enrolled.length == 2 || student.desired_courses < "1"
           next
         end
         if try_enroll(courses, student, request)
           enrolled << request
           open_seats(courses,request)
-          #print_output_files(courses,student,request)
         end #else append to a string
         runnable_class(courses,student,request)
       end
       File.write("output1.csv", "Student ID: #{student.id}\n Enrolled in : #{student.enrollments}\n Reason: N/A\n", mode: 'a')
     end
     print_output_files courses
-    #File.write("output1.csv", "Student ID: #{student.id}\n Enrolled in : #{student.enrollments}\n Reason: N/A\n", mode: 'a')
-    #print_output_files
-    #students
-    #courses
   end
-  def try_enroll (courses, student, courseNum)
+  def try_enroll (courses, student, courseNum) 
+    #makes sure to add the student id for each section based on the max number of courses and makes sure that the course is also added to the students
     course = courses[courseNum]
     if course.nil?
       return false
@@ -79,43 +79,39 @@ class Easyenroll
         section << student.id
         student.enrollments << course.number + "-" + ("%02d" % (i + 1))  #add them into an array
         return true
-      else
-        break
       end
     end
     return false
   end
   def runnable_class  (courses, student, courseNum)
+    #updates if a each section in a class is runnable or not based on if the section meets it's min requirements
     course = courses[courseNum]
     if course.nil?
       return false
     end
-    for i in 0...courses.length
-      if (course.sections.length < course.min)
-        course.can_run = false
-        return true
+    course.sections.each_with_index do |section, i| 
+      if (course.sections[i].length.to_i < course.min)
+        course.can_run[i] = false
       else
-        course.can_run = true
+        course.can_run[i] = true 
       end
-      return false
     end
   end
   def open_seats (courses, courseNum)
-    ####doesn't loop through the number of students in a section
+    #keeps track of how many open seats there are in a section by looping through each course and each section and subtracting the max 
     course= courses[courseNum]
     if course.nil?
       return false
     end
-    course.sections.each_with_index do |section, i| #for the length of each section
-      course.open_sections[i] = course.max - course.sections[i].length  #for i in 0...students.length #section = course.sections[i] #for the students in that section
-      #course.seats_open = course.max - course.sections[i].length  #subtract the max from the length of the section
+    course.sections.each_with_index do |section, i| 
+      course.open_sections[i] = course.max - course.sections[i].length 
     end
   end
   def print_output_files(courses)
+    #creates the courses output file because it is hash we append the results to a string and print them out
     s = ""
     courses.each do |key,course|
       s += "Course Number: " + course.number
-      s += "\nCan Run? " + course.can_run.to_s
       if course.sections.length < 1
         s += "\nNo sections for this course\n"
       end
@@ -123,22 +119,11 @@ class Easyenroll
         s += "\n[Section " + ("%02d" % (i + 1)) + "]\n"
         s += "Number of open seats: " + course.open_sections[i].to_s + "\n"
         s += "Number of filled seats: " + section.length.to_s + "\n"
-        s += "Student Ids in each section: " + section.join(', ') + "\n"
+        s += "Student Ids in each section: " + section.join(', ') + "\n" 
+        s += "Can Run? " + course.can_run[i].to_s + "\n"
       end
       s += "\n"
     end
     File.write("output2.csv", s , mode: 'w+')
   end
 end
-
-File.delete("./output1.csv") if File.exist?("./output1.csv")
-File.delete("./output2.csv") if File.exist?("./output2.csv")
-enroll_students = Easyenroll.new
-puts "Please enter the file name containing all student information "
-studentInfo = gets.chomp   
-#enroll_students.get_students
-puts "Please enter the file name containing all course information "
-courseInfo = gets.chomp
-enroll_students.course_registration(studentInfo,courseInfo)
-
-puts "The output files have been generated as (output1.csv and output2.csv)"
